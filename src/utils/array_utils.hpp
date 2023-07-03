842e07077b38befe37afc23177ccb5ad1e7f4010
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2023-07-03 14:03:55 +0700

    Added documentation for array_utils.hpp

--- src/utils/array_utils.hpp
+++ src/utils/array_utils.hpp
@@ -1,10 +1,30 @@
+/**
+ * \file
+ * \brief Файл, в котором определено пространство имён array_utils.
+ *
+ * \author Alexander Gorbunov
+ * \date 3 июля 2023 года
+ */
+
 #ifndef ANTESTL_BACKEND_ARRAY_UTILS_HPP
 #define ANTESTL_BACKEND_ARRAY_UTILS_HPP
 
 #include <json.hpp>
 
+/**
+ * \brief Пространство имён, в котором определены методы для работы с массивами
+ */
 namespace array_utils {
 
+    /**
+     * \brief Функция, которая позволяет найти индекс требуемого элемента
+     *
+     * \param [in] target_array Указатель на массив, в котором будет производиться поиск элемента
+     * \param [in] array_length Длина массива
+     * \param [in] item Элемент массива, индекс которого требуется найти
+     *
+     * \return Если элемент был найден в массиве, то возвращается его индекс. В противном случае, возвращается -1.
+     */
     template <typename T>
     inline int index(T *target_array, int array_length, T item) {
         for (int pos = 0; pos < array_length; ++pos) {
@@ -16,6 +36,14 @@ namespace array_utils {
         return -1;
     }
 
+    /**
+     * \brief Функция для сравнения величины вложенности двух заданий
+     *
+     * \param [in] j1 Первое сравниваемое задание
+     * \param [in] j2 Второе сравниваемое задание
+     *
+     * \return Возвращает true, если j1 < j2. В противном случае - false;
+     */
     inline bool compare_nested(nlohmann::json j1, nlohmann::json j2) {
         return(j1["nested"].get<int>() < j2["nested"].get<int>());
     }
